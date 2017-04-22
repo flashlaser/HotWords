78f706db2d1d2ff0230759e3fa0228e76a822aef
LittleBei <lovejing0306@gmail.com>
2017-04-22 11:32:25 +0800

    create deal_author.py

---
+++ com/sina/datadeal/deal_author250.py
@@ -0,0 +1,105 @@
+# coding=utf-8
+
+
+def generatorDic(filePath, dicPath):
+    try:
+        f=open(filePath, 'r')
+        dic=open(dicPath, 'w')
+        while 1:
+            line=f.readline()
+            if line:
+                words=line.split('\t')
+                dic.write(words[0]+'\n')
+            else:
+                break;
+    finally:
+        f.close()
+        dic.close()
+
+
+def dealAuthor(targetPath, dicPath, inPath, notInPath, date):
+    try:
+        dic=open(dicPath, 'r')
+        dicList=[]
+        while 1:
+            line=dic.readline()
+            if line:
+                words=line.split('\t')
+                dicList.append(words[0])
+            else:
+                break
+
+
+        target = open(targetPath, 'r')
+        authorIn=open(inPath, 'w')
+        authorNotIn=open(notInPath, 'w')
+
+        while 1:
+            line=target.readline()
+            if line:
+                words=line.split('\t')
+                if words[0]==date:
+                    if words[2] in dicList:
+                        authorIn.write(words[2]+'\n')
+                    else:
+                        authorNotIn.write(words[2]+'\n')
+                else:
+                    pass
+            else:
+                break
+    finally:
+        if dic:
+            dic.close()
+        if target:
+            target.close()
+        authorIn.close()
+        authorNotIn.close()
+
+def dealAuthor(targetPath, dicPath, inPath, notInPath):
+    try:
+        dic=open(dicPath, 'r')
+        dicList=[]
+        while 1:
+            line=dic.readline()
+            if line:
+                words=line.split('\t')
+                dicList.append(words[0])
+            else:
+                break
+
+
+        target = open(targetPath, 'r')
+        authorIn=open(inPath, 'w')
+        authorNotIn=open(notInPath, 'w')
+
+        while 1:
+            line=target.readline()
+            if line:
+                words=line.split('\t')
+                if words[0] in dicList:
+                    authorIn.write(words[0]+'\n')
+                else:
+                    authorNotIn.write(words[0]+'\n')
+            else:
+                break
+    finally:
+        if dic:
+            dic.close()
+        if target:
+            target.close()
+        authorIn.close()
+        authorNotIn.close()
+
+
+if __name__=='__main__':
+
+    targetPath='../../../data/author250/20170421.blog'
+    dicPath='../../../data/author250/all_21.txt'
+    inPath='../../../data/author250/in_21.txt'
+    notInPath='../../../data/author250/not_in_21.txt'
+    #path='../../../data/author250/dic.txt'
+
+    #dealAuthor(targetPath, dicPath,inPath, notInPath, '20170328')
+    #generatorDic(dicPath, path)
+    dealAuthor(targetPath, dicPath, inPath, notInPath)
+    print('hi')
