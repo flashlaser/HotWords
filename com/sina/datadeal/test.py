748a42f30aa9b2ebe35a4ae11b37c42fe1518f26
LittleBei <lovejing0306@gmail.com>
2017-04-22 11:34:09 +0800

    update test.py

--- com/sina/datadeal/test.py
+++ com/sina/datadeal/test.py
@@ -1,6 +1,15 @@
 # coding=utf-8
 
-
+def splitSpammer(filePath):
+    f=open(filePath, 'r')
+    list=[]
+    for i in range(10):
+        line=f.readline()
+        words=line.split('\t')
+        print(line)
+        print(words[0])
 
 if __name__=='__main__':
     print('hello')
+    filePath='../../../data/author250/all_28.txt'
+    splitSpammer(filePath)
