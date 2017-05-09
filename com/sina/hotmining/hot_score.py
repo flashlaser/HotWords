406d2598d654a2197b5f8ca70bc3141b0da9719b
LittleBei <lovejing0306@gmail.com>
2017-05-09 19:14:12 +0800

    create hotmining

--- com/sina/hotmining/hot_score.py
+++ com/sina/hotmining/hot_score.py
@@ -17,23 +17,23 @@ def datas_load(file_path):
         words = line.strip().strip('\n').split('\t')
         # print(len(words))
         mid = words[0]
-        uid = words[1]
-        url = words[2]
-        level = words[3]
-        reads = float(words[4])
-        reposts = float(words[5])
-        attitudes = float(words[6])
-        comments = float(words[7])
-        action = float(words[8])
-        expose = float(words[9])
-        rate = float(words[10])
+        # uid = words[1]
+        # url = words[2]
+        # level = words[3]
+        reads = float(words[1])
+        reposts = float(words[2])
+        attitudes = float(words[3])
+        comments = float(words[4])
+        action = float(words[5])
+        expose = float(words[6])
+        rate = float(words[7])
 
         data_use = []
         data_nouse = []
         data_nouse.append(mid)
-        data_nouse.append(uid)
-        data_nouse.append(url)
-        data_nouse.append(level)
+        # data_nouse.append(uid)
+        # data_nouse.append(url)
+        # data_nouse.append(level)
 
         data_use.append(reads)
         data_use.append(reposts)
@@ -41,8 +41,7 @@ def datas_load(file_path):
         data_use.append(comments)
         data_use.append(action)
         data_use.append(expose)
-
-        data_nouse.append(rate)
+        data_use.append(rate)
         # print(data)
         datas_use.append(data_use)
         datas_nouse.append(data_nouse)
@@ -59,7 +58,7 @@ def datas_min_max_scaler(datas):
 
 ## 计算每条微博的分数
 def cal_hot_score(datas_min_max, reads_weight, reposts_weight, attitudes_weight, comments_weight, action_weight,
-                  expose_weight):
+                  expose_weight, rate_weight=0):
     datas_score = []
     for data in datas_min_max:
         reads = data[0]
@@ -68,7 +67,8 @@ def cal_hot_score(datas_min_max, reads_weight, reposts_weight, attitudes_weight,
         comments = data[3]
         action = data[4]
         expose = data[5]
-        score = reads_weight * reads + reposts_weight * reposts + attitudes_weight * attitudes + comments_weight * comments + action_weight * action + expose_weight * expose
+        rate = data[6]
+        score = reads_weight * reads + reposts_weight * reposts + attitudes_weight * attitudes + comments_weight * comments + action_weight * action + expose_weight * expose + rate * rate_weight
 
         data.append(score)
         datas_score.append(data)
@@ -76,29 +76,30 @@ def cal_hot_score(datas_min_max, reads_weight, reposts_weight, attitudes_weight,
 
 
 ## 对没有用到的数据和评分后的数据进行合并和排序（依据分数score进行排序）
-def datas_merge_sort(datas_nouse, datas_score):
+def datas_merge_sort(datas_nouse, datas_use, datas_score):
     datas = []
     datas_size = len(datas_score)
     for i in range(0, datas_size):
         mid = datas_nouse[i][0]
-        uid = datas_nouse[i][1]
-        url = datas_nouse[i][2]
-        level = datas_nouse[i][3]
-        rate = datas_nouse[i][4]
+        # uid = datas_nouse[i][1]
+        # url = datas_nouse[i][2]
+        # level = datas_nouse[i][3]
 
-        reads = datas_score[i][0]
-        reposts = datas_score[i][1]
-        attitudes = datas_score[i][2]
-        comments = datas_score[i][3]
-        action = datas_score[i][4]
-        expose = datas_score[i][5]
-        score = datas_score[i][6]
+        reads = datas_use[i][0]
+        reposts = datas_use[i][1]
+        attitudes = datas_use[i][2]
+        comments = datas_use[i][3]
+        action = datas_use[i][4]
+        expose = datas_use[i][5]
+        rate = datas_use[i][6]
+
+        score = datas_score[i][7]
 
         data = []
         data.append(mid)
-        data.append(uid)
-        data.append(url)
-        data.append(level)
+        # data.append(uid)
+        # data.append(url)
+        # data.append(level)
         data.append(reads)
         data.append(reposts)
         data.append(attitudes)
@@ -109,8 +110,8 @@ def datas_merge_sort(datas_nouse, datas_score):
         data.append(score)
 
         datas.append(data)
-
-    datas.sort(key=lambda x: x[11], reverse=True)
+    # 对合并后的结构进行排序
+    datas.sort(key=lambda x: x[8], reverse=True)
 
     return datas
 
@@ -123,7 +124,7 @@ def datas_out(datas_out_path, datas, n=20):
 
 
 if __name__ == '__main__':
-    datas_path = '/home/littlebei/program/python/pycharm/HotWords/data/dump/result/hot_20170502_15'
+    datas_path = '/home/littlebei/program/python/pycharm/HotWords/data/hotming/result/20170504/method1_users_c1-c4_blog_20170504_08.txt'
     datas_use, datas_nouse = datas_load(datas_path)
     print(datas_use[0])
 
@@ -133,11 +134,11 @@ if __name__ == '__main__':
     datas_score = cal_hot_score(datas_min_max, 0.4, 0.35, 0.05, 0.05, 0.1, 0.05)
     print(datas_score[0])
 
-    datas_sort = datas_merge_sort(datas_nouse, datas_score)
+    datas_sort = datas_merge_sort(datas_nouse, datas_use, datas_score)
     print(len(datas_sort))
 
     datas_df = pd.DataFrame(datas_sort)
     print(datas_df.head(5))
 
-    datas_out_path = '/home/littlebei/program/python/pycharm/HotWords/data/dump/result/hot_score_20170502_15.txt'
-    datas_out(datas_out_path, datas_sort)
+    datas_out_path = '/home/littlebei/program/python/pycharm/HotWords/data/hotming/result/20170504/20170504_08.blog_score'
+    datas_out(datas_out_path, datas_sort, n=len(datas_sort))
