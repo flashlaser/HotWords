4219553cef1f05ff42dca2419bbb32751c37583f
LittleBei <lovejing0306@gmail.com>
2017-05-17 10:50:35 +0800

    update deal_user_filter.py

---
+++ com/sina/dealdata/deal_user_filter.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+import re
+import time
+import urllib
+import requests
+
+
+if __name__ == '__main__':
+    out_file=open('/home/littlebei/program/python/pycharm/HotWords/data/hotming/users_level_filter', 'w')
+    users_blank_set=set()
+
+    for line in open('/home/littlebei/program/python/pycharm/HotWords/data/hotming/user_black_uid.dat', 'r').readlines():
+        uid=line.strip().strip('\n')
+        users_blank_set.add(uid)
+
+    for line in open('/home/littlebei/program/python/pycharm/HotWords/data/hotming/users_level', 'r').readlines():
+        line=line.strip().strip('\n')
+        items=line.split('\t')
+        uid=items[0]
+        level=items[1]
+        if uid in users_blank_set:
+            pass
+        else:
+            out_file.write(line+'\n')
+    out_file.close()
