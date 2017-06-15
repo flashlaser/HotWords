1ab90f57cd65fdb2d50987dd9b8be240a1724a66
LittleBei <lovejing0306@gmail.com>
2017-06-15 20:13:58 +0800

    update deal_user_filter.py

--- com/sina/dealdata/deal_user_filter.py
+++ com/sina/dealdata/deal_user_filter.py
@@ -6,14 +6,14 @@ import requests
 
 
 if __name__ == '__main__':
-    out_file=open('/home/littlebei/program/python/pycharm/HotWords/data/hotmining/users_level_filter', 'w')
+    out_file=open('/data0/shaojie5/littlebei/program/python/pycharm/HotWords/data/hotmining/users_level_filter', 'w')
     users_blank_set=set()
 
-    for line in open('/home/littlebei/program/python/pycharm/HotWords/data/hotmining/user_black_uid.dat', 'r').readlines():
+    for line in open('/data0/shaojie5/littlebei/program/python/pycharm/HotWords/data/hotmining/user_black_uid.dat', 'r').readlines():
         uid=line.strip().strip('\n')
         users_blank_set.add(uid)
 
-    for line in open('/home/littlebei/program/python/pycharm/HotWords/data/hotmining/users_level', 'r').readlines():
+    for line in open('/data0/shaojie5/littlebei/program/python/pycharm/HotWords/data/hotmining/users_level', 'r').readlines():
         line=line.strip().strip('\n')
         items=line.split('\t')
         uid=items[0]
